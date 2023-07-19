45be2b6f9452ad5a8a47e450f051395fb8ddd570
Jukka Snellman <jusne@eg.fi>
2023-07-19 13:08:40 +0300

    add LoggerExtensions

---
+++ ScopedLogger/LoggerExtensions.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Logging;
+
+namespace ScopedLogger;
+
+/// <summary>
+/// Extensions for <see cref="ILogger"/>.
+/// </summary>
+public static class LoggerExtensions
+{
+    public static IScopedLogger Chain(this ILogger logger) =>
+        logger as IScopedLogger ?? new ScopedLogger(logger, new Dictionary<string, object>());
+    
+    public static IScopedLogger ForContext(this ILogger logger, string key, object value) =>
+        logger.Chain().ForContext(key, value);
+}
